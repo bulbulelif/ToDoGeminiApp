de34c0df2563affd16056df5d7a4cfd5cc01a20e
bulbulelif <elifbulbulofficial@gmail.com>
2025-07-29 17:58:27 +0300

    render

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from starlette.responses import RedirectResponse
+from starlette import status
 from models import Base, Todo
 from database import engine
 from routers.auth import router as auth_router
@@ -8,5 +10,12 @@ from routers.todo import router as todo_router
 app = FastAPI()
 app.include_router(auth_router)
 app.include_router(todo_router)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+def read_root(request: Request):
+    return RedirectResponse("/todo/todo-page", status_code= status.HTTP_302_FOUND)
+
 
 Base.metadata.create_all(bind=engine)
--- routers/auth.py
+++ routers/auth.py
@@ -10,11 +10,14 @@ from sqlalchemy.orm import Session, defer
 from starlette import status
 from jose import jwt, JWTError
 from datetime import timedelta, timezone, datetime
+from fastapi.templating import Jinja2Templates
 
 router = APIRouter(
     prefix="/auth",
     tags=["Authentication"]
 )
+
+templates = Jinja2Templates(directory="templates")
 
 SECRET_KEY = "0knrjhltrgbldsveowvzgqitgt1ibcb4y4107lixzda4g9j89cdvvpeo71b71wrf"
 ALGORITHM = "HS256"
@@ -75,6 +78,16 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
 
     except JWTError:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token is invalid")
+
+
+@router.get("/login-page")
+def render_login_page(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
+
+@router.get("/register-page")
+def render_register_page(request: Request):
+    return templates.TemplateResponse("register.html", {"request": request})
+
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
